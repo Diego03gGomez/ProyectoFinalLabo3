d6207dd001856ae38fdea9cabe9a6ce39b08e821
Diego <ezequielgomezdie@gmail.com>
2023-06-15 10:14:17 -0300

    Modifique el como se ven los juegos

--- LaboFinal/js/components/game.js
+++ LaboFinal/js/components/game.js
@@ -2,10 +2,15 @@ export function createGame(game) {
   const gameEl = document.createElement("div");
   gameEl.classList.add("game");
   gameEl.innerHTML = `
+<div id="cards">
+
       <img src="${game.background_image}" alt="${game.name}" class="game-img" />
       <div class="game-info">
         <h3>${game.name}</h3>
         <span>${game.released}</span>
+      </div>
+
+
       </div>
     `;
   return gameEl;
